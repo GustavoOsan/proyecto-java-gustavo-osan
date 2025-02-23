5d2f8a5ebc3e1ae2dfd96a4ea9e7c8e52707f368
Gustavo Osan <gustavoosan@gmail.com>
2025-02-23 16:19:24 -0300

    Primera carga

---
+++ js/index.js
@@ -0,0 +1,59 @@
+const PRODUCTOS = [
+
+    {
+        id: 1,
+        nombre: "Producto 1",
+        precio: 100,
+        imagen: "./Asets/imagenes/1.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Producto 2",
+        precio: 200,
+        imagen: "https://via.placeholder.com/150"
+    },
+    {
+        id: 3,
+        nombre: "Producto 3",
+        precio: 200,
+        imagen: "https://via.placeholder.com/150"
+    },
+    {
+        id: 4,
+        nombre: "Producto 4",
+        precio: 200,
+        imagen: "https://via.placeholder.com/150"
+    },
+    {
+        id: 5,
+        nombre: "Producto 5",
+        precio: 200,
+        imagen: "https://via.placeholder.com/150"
+    },
+    {
+        id: 6,
+        nombre: "Producto 6",
+        precio: 200,
+        imagen: "https://via.placeholder.com/150"
+    },
+    {
+        id: 7,
+        nombre: "Producto 7",
+        precio: 200,
+        imagen: "https://via.placeholder.com/150"
+    },
+    {
+        id: 8,
+        nombre: "Producto 8",
+        precio: 200,
+        imagen: "https://via.placeholder.com/150"
+    },
+    {
+        id: 9,
+        nombre: "Producto 9",
+        precio: 200,
+        imagen: "https://via.placeholder.com/150"
+    },
+
+
+]
